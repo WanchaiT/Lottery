1a1b2581c844efa2117367b8f826d753e9ac8b81
Wanchai Thiantanawat <wanchairoot@WanMac.local>
2018-08-18 13:49:11 +0700

    d

---
+++ lottery.py
@@ -0,0 +1,21 @@
+from random import randint
+
+def showStatIfWin():
+
+def shoeStat():
+
+def checkLottery(myNumber ,numRandom):
+    if(myNumber == numRandom):
+        showStatIfWin()
+        return true
+    else:
+        return false
+
+def randomNum():
+    return random(0,999999)
+
+def main():
+    cost = 0
+    myNumber = int(input())
+    mount = ['JAN','FEB','MAR','APR','MAY','JUN',
+            'JUL','AUG','SEP','OCT','NOV','DEC']
